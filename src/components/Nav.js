18b49f6a39f0aadc53d7a4b46aa307ff7d3ea9a5
Elizabeth <tjeudy.e@gmail.com>
2023-04-12 22:03:38 -0400

    created home, destination, crew, and technology components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,15 @@
-function Nav() {}
+import Home from "./Home";
+import Destination from "./Destination";
+import Crew from "./Crew";
+import Technology from "./Technology";
+
+function Nav() {
+	<nav className="nav">
+		<Home />
+		<Destination />
+		<Crew />
+		<Technology />
+	</nav>;
+}
 
 export default Nav;
