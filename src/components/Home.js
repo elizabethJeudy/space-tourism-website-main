2cd5d44d5ce03a97fc1bb4d84f3dcce30e23dfb2
Elizabeth <tjeudy.e@gmail.com>
2023-04-15 20:58:53 -0400

    imported background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
+import React from "react";
 import homeDesktop from "../starter-code/assets/home/homeDesktop.jpg";
+import "../css components/Home.css";
 
 function Home() {
-	return <div style={{ backgroundImage: `url(${homeDesktop})` }}></div>;
+	const bgImage = {
+		backgroundImage: `url(${homeDesktop})`,
+		backgroundPosition: "center",
+		backgroundSize: "cover",
+		backgroundRepeat: "no-repeat",
+	};
+	return (
+		<div className="Home">
+			<div className="background" style={bgImage}></div>
+		</div>
+	);
 }
 
 export default Home;
