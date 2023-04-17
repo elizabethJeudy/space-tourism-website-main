d7a8be285212e93606cfcc4562b6bd5563e7b797
Elizabeth <tjeudy.e@gmail.com>
2023-04-16 22:51:16 -0400

    imported background image and nav component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import homeDesktop from "../starter-code/assets/home/homeDesktop.jpg";
 import "../css components/Home.css";
+import Nav from "./Nav";
+/*
+const bgImage = {
+	backgroundImage: `url(${homeDesktop})`,
+	backgroundPosition: "center",
+	backgroundSize: "cover",
+	backgroundRepeat: "no-repeat",
+}
+*/
 
 function Home() {
-	const bgImage = {
-		backgroundImage: `url(${homeDesktop})`,
-		backgroundPosition: "center",
-		backgroundSize: "cover",
-		backgroundRepeat: "no-repeat",
-	};
 	return (
-		<div className="Home">
-			<div className="background" style={bgImage}></div>
+		<div style={{ backgroundImage: `url(${homeDesktop})` }}>
+			<Nav />
 		</div>
 	);
 }
